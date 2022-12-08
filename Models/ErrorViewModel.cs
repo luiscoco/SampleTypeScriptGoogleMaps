171e4dfabd1ac52dcf97e076579523c290150f92
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2022-12-08 15:16:37 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleTypeScript.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
